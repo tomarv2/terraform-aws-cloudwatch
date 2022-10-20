70f8b1134bff2e316b0f26b7c2a4c17c28acbc1a
tomarv2 <varuntomar2019@gmail.com>
2022-10-19 21:55:13 -0700

    testing running in parrallel for non aws env

--- examples/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "log_group" {
-  description = "Cloudwatch log group name"
-  value       = module.cloudwatch.log_group_name
-}
-
-output "cloudwatch_log_group_arn" {
-  description = "Cloudwatch log group ARN"
-  value       = module.cloudwatch.log_group_arn
-}
--- outputs.tf
+++
@@ -1,9 +0,0 @@
-output "log_group_name" {
-  description = "Cloudwatch log group name"
-  value       = join("", aws_cloudwatch_log_group.cloudwatch.*.name)
-}
-
-output "log_group_arn" {
-  description = "Cloudwatch log group ARN"
-  value       = join("", aws_cloudwatch_log_group.cloudwatch.*.arn)
-}
